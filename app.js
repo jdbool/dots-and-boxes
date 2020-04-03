2713a6b797148ef896045276b41d3909292994d3
jdb <44955015+jdbool@users.noreply.github.com>
2020-04-03 12:59:05 -0700

    Enable caching and compression

--- app.js
+++ app.js
@@ -5,13 +5,15 @@ const path = require('path');
 const express = require('express');
 const app = express();
 const http = require('http').createServer(app);
+const compression = require('compression');
 
 const { sequelize } = require('./database');
 require('./server')(http);
 
 app.set('trust proxy', 'loopback');
 
-app.use(express.static(path.join(__dirname, 'game')));
+app.use(compression());
+app.use(express.static(path.join(__dirname, 'game'), { maxAge: '7d' }));
 
 (async () => {
 	const { PORT } = process.env;
